03e3b72504a9568bdb8f8488a4d1b59a9a9260a7
Rahul Shendre <144231863+rahulshendre@users.noreply.github.com>
2025-01-15 21:52:19 +0530

    Time: 2786 ms (5.04%) | Memory: 34.2 MB (90.73%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -5,29 +5,29 @@ public:
         // arrays whose sum equals k
         // generate all subarrays and thier sum, and return number of arrays whose sum 
         // is greater than k 
-        // int count = 0;
-        // int n = nums.size();
-
-        // for(int i = 0; i < nums.size(); i++){
-        //     int sum = 0;
-        //     for(int j = i; j < nums.size(); j++){
-        //         sum = sum + nums[j];
-        //         if(sum == k){
-        //             count++;
-        //         }
-        //     }
-        // }
-        // return count;
-        unordered_map<int, int> prefixSumCount = {{0,1}};
-        int currentSum = 0;
         int count = 0;
+        int n = nums.size();
 
-        for(int num : nums)
-        {
-            currentSum = currentSum + num;
-            count = count + prefixSumCount[currentSum - k];
-            prefixSumCount[currentSum]++;
+        for(int i = 0; i < nums.size(); i++){
+            int sum = 0;
+            for(int j = i; j < nums.size(); j++){
+                sum = sum + nums[j];
+                if(sum == k){
+                    count++;
+                }
+            }
         }
         return count;
+        // unordered_map<int, int> prefixSumCount = {{0,1}};
+        // int currentSum = 0;
+        // int count = 0;
+
+        // for(int num : nums)
+        // {
+        //     currentSum = currentSum + num;
+        //     count = count + prefixSumCount[currentSum - k];
+        //     prefixSumCount[currentSum]++;
+        // }
+        // return count;
     }
 };
