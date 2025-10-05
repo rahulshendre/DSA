981b5d2869f2896ae2d2d193c2be7d6412580198
Rahul Shendre <144231863+rahulshendre@users.noreply.github.com>
2025-10-04 21:43:56 -0700

    Time: 0 ms (100.00%) | Memory: 31.2 MB (80.02%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -5,16 +5,16 @@ public:
         int right = nums.size() - 1;
 
         while(left <= right){
-            int mid = left + (right - left) / 2;
-
-            if(nums[mid] == target){
-                return mid;
-            } 
-            if(nums[mid] < target){
+           int mid = (right + left)/2;
+           
+            if(nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target){
                 left = mid + 1;
             }
             else{
-                right = mid - 1;
+                return mid;
             }
         }
         return -1;
