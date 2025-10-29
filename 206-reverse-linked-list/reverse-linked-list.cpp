530d2d0778e50a57ceadbccaa6b4fc2eaeaec58d
Rahul Shendre <144231863+rahulshendre@users.noreply.github.com>
2025-10-29 04:21:48 -0700

    Time: 0 ms (100.00%) | Memory: 13.4 MB (39.30%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -11,17 +11,40 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        ListNode* next = nullptr;
+        ListNode* node = nullptr;
 
-        while(curr != nullptr){
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        while(head != nullptr){
+            ListNode* temp;
+            temp = head->next;
+            head->next = node;
+            node = head;
+            head = temp;
         }
-        return prev;
-
+        return node;
     }
 };
+//what we do to reverse linked list is add a node which is essentially a nullptr here because here we need the number to be able to store address of others too
+/* so what we did here was 
+eg: 1->2->nullptr
+made a temp = stored head->next which is 2
+then we said head->next = node=nullptr
+then we said that head = node
+bringing a empty nullptr ahead of 1 
+then we say that head = temp temp had the value 2 which is now stored in the head, which was empty before 
+so now we have 2->1->nullptr
+
+*/
+
+
+//how to swap using temp
+/* suppose you have two numbers a and b, you want to swap them 
+now you take the value of a and store in a temporary variable
+ok.
+now you make a and b equal so now a has the value of b
+correct, so essentially both are equal and are now equal to b
+now you give b the value of temp
+which was a previously
+great 
+now a has the value of b and b has the value of a 
+simply beacuse we stored the value of a in temp for some time
+*/
