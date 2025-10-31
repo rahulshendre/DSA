9ebd3e3bef5bf44f38f45b99f8ce0ba7c4cb4a33
Rahul Shendre <144231863+rahulshendre@users.noreply.github.com>
2025-10-31 22:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 25.3 MB (86.35%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -1,16 +1,62 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> freq(n,0);
-        vector<int> res;
+        //all the 4 ways to do it 
+        //1st - sort - O(nlogn)
 
-        for(int num : nums){
-            freq[num]++;
-            if(freq[num] == 2){
-                res.push_back(num);
-            }
+        // sort(nums.begin(), nums.end());
+        // vector<int> res;
+        // for(int i = 1; i < nums.size(); i++){
+        //     if(nums[i] == nums[i-1]){
+        //         res.push_back(nums[i]);
+        //     }
+        // }
+        // return res;
+
+        //2nd - hash map
+        // unordered_map<int, int>mpp;
+        // vector<int> res;
+        // for(int num : nums){
+        //     if(mpp.count(num)){
+        //         res.push_back(num);
+        //     }
+        //     else{
+        //         mpp[num] == 1;
+        //     }
+        // }
+        // return res;
+
+        //3rd - array which stores the freq 
+        // vector<int> res;
+        // int n = nums.size();
+        // vector<int> freq(n, 0);
+
+        // for(int num : nums){
+        //     freq[num]++;
+        //     if(freq[num] == 2){
+        //         res.push_back(num);
+        //     }
+        // }
+        // return res;
+
+        //4th - xor
+        int XOR = 0;
+        int n = nums.size() - 2;
+
+        for (int num : nums) XOR ^= num;
+        for (int i = 0; i < n; i++) XOR ^= i;
+
+        int diffBit = XOR & -XOR;
+
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & diffBit) == 0) a ^= num;
+            else b ^= num;
         }
-        return res;
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) a ^= i;
+            else b ^= i;
+        }
+        return {a, b};
     }
 };
