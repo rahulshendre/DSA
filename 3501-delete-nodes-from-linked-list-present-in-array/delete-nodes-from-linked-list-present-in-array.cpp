48c80d4c02e16d8daef799b4f3067e937f3b7d99
Rahul Shendre <144231863+rahulshendre@users.noreply.github.com>
2025-11-01 22:03:58 +0530

    Time: 95 ms (71.38%) | Memory: 263.4 MB (58.72%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,24 +11,41 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_map<int,int> mpp;
+        // unordered_map<int,int> mpp;
 
-        for(int num : nums){
-            mpp[num]++;
+        // for(int num : nums){
+        //     mpp[num]++;
+        // }
+        // ListNode* dummy = new ListNode(0, head);
+        // ListNode* prev = dummy;
+        // ListNode* curr = head;
+
+        // while(curr != nullptr){
+        //     if(mpp.count(curr->val)){
+        //         prev -> next = curr -> next;
+        //     }
+        //     else{
+        //         prev = curr;
+        //     }
+        //     curr = curr->next;
+        // }
+        // return dummy->next;
+
+        unordered_set<int> mpp(nums.begin(), nums.end());
+
+        while(head && mpp.count(head->val)){
+            head = head->next;
         }
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* prev = dummy;
         ListNode* curr = head;
-
-        while(curr != nullptr){
-            if(mpp.count(curr->val)){
-                prev -> next = curr -> next;
-            }
-            else{
-                prev = curr;
+        while(curr && curr->next){
+            //upar wala is just talkig about exitence of the lenght
+            //this lower ones will tell us, if that thing we have tells
+            // 
+            while(curr->next && mpp.count(curr->next->val)){
+                curr->next = curr->next->next;
             }
             curr = curr->next;
         }
-        return dummy->next;
+        return head;
     }
 };
